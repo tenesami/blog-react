d4df08c1dced1194fc366aa961bbf26a9f2816b4
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-08-25 17:33:42 -0600

    create content to each page

---
+++ src/pages/Article.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Article = () => (
+    <>
+    <h1>Hello, welcome to my article!</h1>
+    <p>
+        Welcome to my blog! Proin congue
+        ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+        Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+        non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+        eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+        sodales purus euismod.
+    </p>
+    
+    </>
+);
+
+export default Article;
