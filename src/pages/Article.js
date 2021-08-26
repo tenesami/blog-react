14b130f2ca068a4aeb32d3b422b22a4c6c181e09
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-08-25 21:07:37 -0600

    desplay other article under the current article

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import articleContent from './ArticleContent';
+import ArticleList from '../Components/ArticleList';
+
 const Article = ({match}) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name)
 
     if(!article) return <h1>Article Does not Exist!</h1>
+
+    //create other related articles under each article 
+    const otherArticles = articleContent.filter(article => article.name !== name);
     return (
         <>
         <h1>Welcome to the {article.title} my article!</h1>
         {article.content.map((para, key) => (
             <p>{para}</p>
         ))}
+
+        <h3>Other Aricles: </h3>
+        <ArticleList articles={otherArticles}/>
         </>
     );
     }
