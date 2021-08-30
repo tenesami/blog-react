5f72845ef1f75828f1ac7954d1ca0f474e80a784
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-08-29 18:03:51 -0600

    fix the text for practice

--- src/pages/About.js
+++ src/pages/About.js
@@ -25,7 +25,7 @@ const About = () => (
         nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
         sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
         interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-        consequat quam. Vivamus accumsan dui in facilisis aliquet.,
+        consequat quam. Vivamus accumsan facilisis aliquet.,
     </p>
     </>
 );
